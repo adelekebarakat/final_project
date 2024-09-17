960c2bf24bb637eef65858d842e5937759de21fe
Adeleke Barakat <adelekebarakat96@gmail.com>
2024-09-17 16:43:50 +0100

    Database Change

--- blooddatabase/settings.py
+++ blooddatabase/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = 'blooddatabase.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    # 'default': dj_database_url.config(default=config('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
